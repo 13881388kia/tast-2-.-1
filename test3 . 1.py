717fac08a4fe745007714be9bd159cbee741d21c
13881388kia <95403139+13881388kia@users.noreply.github.com>
2021-12-13 15:16:11 +0330

    Add files via upload

---
+++ test3 . 1.py
@@ -0,0 +1,13 @@
+import math
+
+r = int(input("shoa ra vared kon:"))
+
+# a = r * r * 3.14
+# a = r **2 * 3.14
+# p = 2 * r * 3.14
+a = r **2 * math.pi
+p = 2 * r * math.pi
+
+print(math.pi)
+
+print("masahat:",a,"mohit",p)
